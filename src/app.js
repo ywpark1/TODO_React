60faa9c3223e3c129a360ebd34ef3b58c2bc8945
Yeonwoo Park <ywoo1234@gmail.com>
2018-09-23 13:10:25 -0400

    Revert to IndecisionApp

--- src/app.js
+++ src/app.js
@@ -2,14 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const Layout = () => {
-    return (
-        <div>
-            <p>header</p>
-            <p>footer</p>
-        </div>
-    );
-    
-}
-
-ReactDOM.render(<Layout />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
