1511f884d7fc2e7e2c409b5ce01dd5478b786c63
Yeonwoo Park <ywoo1234@gmail.com>
2018-09-23 12:58:42 -0400

    Refactoring Arrow functions - simplify by removing return

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.optionText}
-            <button onClick={(e) => {
-                props.handleDeleteOption(props.optionText);
-            }}
-            >Remove</button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        {props.optionText}
+        <button onClick={(e) => {
+            props.handleDeleteOption(props.optionText);
+        }}
+        >Remove</button>
+    </div>
+);
 
 export default Option;
