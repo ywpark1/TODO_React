309a19c23ae6b93853eb7e758d33d906d9a03704
Yeonwoo Park <ywoo1234@gmail.com>
2018-09-23 15:37:16 -0400

    Disabled autocomplete on the input form

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -32,7 +32,7 @@ export default class AddOption extends React.Component {
         return (
             <div>
                 { this.state.error && <p className="add-option-error">{this.state.error}</p> }
-                <form className="add-option" onSubmit={this.handleAddOption}>
+                <form className="add-option" onSubmit={this.handleAddOption} autocomplete="off">
                     <input className="add-option__input" type="text" name="option" />
                     <button className="button">Add Option</button>
                 </form>
