98c29b271fc3bfec8ef83f0caef32ac4f3f23e95
Yeonwoo Park <ywoo1234@gmail.com>
2018-09-23 13:43:12 -0400

    Add missing comma

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 test: /\.scss/,
                 use: [
                     { loader: "style-loader" },
-                    { loader: "css-loader" }
+                    { loader: "css-loader" },
                     { loader: "sass-loader" }
                 ]
             }
